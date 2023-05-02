ddec816a2b6edf7314ffabdb9097f7cfb20221f8
Chaosheng1989 <yanzihebe@gmail.com>
2023-05-02 17:55:44 +0800

    update #滑動效果

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,18 @@
+//點擊漢堡選單時展開
 $(document).ready(function() {
   $('.menu-icon').click(function() {
     $('.menu').toggleClass('menu-show');
     document.querySelector('.banner').classList.toggle('banner-show');
   });
+});
+
+//點擊#產生滑動
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+          scrollTop: target.offset().top
+      }, 1000);
+  }
 });
