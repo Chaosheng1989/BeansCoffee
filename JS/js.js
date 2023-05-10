e6754ef831a5d023dafb7887939b084e5c6715ec
Chaosheng1989 <yanzihebe@gmail.com>
2023-05-10 15:33:24 +0800

    update

--- JS/js.js
+++ JS/js.js
@@ -17,6 +17,23 @@ $('a[href^="#"]').on('click', function(event) {
   }
 });
 
-$(document).ready(function () {
-  
+//spec-btn 按鈕點選顏色變更
+// 所有的.spec-btn按鈕元素
+const specBtns = document.querySelectorAll('.spec-btn');
+
+// 為每個按鈕添加點擊事件監聽器
+specBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // 如果按鈕已經是活躍狀態，則刪除active類
+    if (btn.classList.contains('active')) {
+      btn.classList.remove('active');
+    } 
+    // 如果按鈕不是活躍狀態，則從所有按鈕中刪除active類，並將其添加到當前點擊的按鈕中
+    else {
+      specBtns.forEach(otherBtn => {
+        otherBtn.classList.remove('active');
+      });
+      btn.classList.add('active');
+    }
+  });
 });
